3598a40ef0dc12fcc94bb0ed45f969dc6fe48a10
Daniel Myers <64658614+Danluk1001@users.noreply.github.com>
2025-10-29 18:31:34 -0400

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,17 @@
+public class Tree {
+    public static void main(String[] args) {
+
+    //This task will plant a Tree.
+    
+    System.out.println("Hey there, I’m Daniel and I’m learning to code in Java!");
+
+    System.out.println("I’m going to plant a tree today!” or “Ready to get my hands dirty!");
+
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");   
+
+    }
+}
